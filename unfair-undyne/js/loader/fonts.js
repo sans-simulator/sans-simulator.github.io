ff8bdca570bf0e34e2f848f30ff72562f2c242eb
sans-simulator <gulshan99507@gmail.com>
2025-08-03 09:38:58 +0530

    Update fonts.js

--- unfair-undyne/js/loader/fonts.js
+++ unfair-undyne/js/loader/fonts.js
@@ -3,9 +3,9 @@
 var fonts_loader = new PIXI.loaders.Loader();
 
 fonts_loader
-    .add("undertale", "../fonts/undertale.fnt")
-    .add("numbers", "../fonts/numbers.fnt")
-    .add("speechbubble", "../fonts/speechbubble.fnt")
+    .add("undertale", "/unfair-undyne/fonts/undertale.fnt")
+    .add("numbers", "/unfair-undyne/fonts/numbers.fnt")
+    .add("speechbubble", "/unfair-undyne/fonts/speechbubble.fnt")
     .on("complete", function(loader, resources) {
         process_fonts(resources);
     });
